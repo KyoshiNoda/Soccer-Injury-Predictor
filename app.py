07218fc21cab8a88bd00a4669a5829876ea4c7f2
kyoshinoda <kyoshisew@gmail.com>
2024-04-28 22:14:32 -0400

    added data.txt

--- app.py
+++ app.py
@@ -6,4 +6,23 @@ load_dotenv()
 
 prem_df = league_player_data("EPL", "2022")
 salah = prem_df.iloc[3]
-# print(get_weather_forecast())
+
+
+# Sample data as a string
+data_str = """
+[(u'238223', u'Ederson', u'Manchester City', u'1,14 Bill. €', u'1993-08-17', u'89', u'1,88\xa0', u'Brazil', u'Goalkeeper', u'left', [(u'Benfica ', u'Man City ', u'17/18', u'Jul 1, 2017'), (u'Rio Ave FC ', u'Benfica ', u'15/16', u'Jul 1, 2015')], [(u'16/17', u'Meniscal Injury', u'29')]),
+ (u'40423', u'Claudio Bravo', u'Manchester City', u'1,14 Bill. €', u'1983-04-13', u'84', u'1,84\xa0', u'Chile', u'Goalkeeper', u'right', [(u'FC Barcelona ', u'Man City ', u'16/17', u'Aug 25, 2016')], [(u'18/19', u'Achilles tendon rupture', u'314')])]
+"""
+
+parsed_data = parse_data(data_str)
+
+df_players = create_players_dataframe(parsed_data)
+df_transfers = create_transfers_dataframe(parsed_data)
+df_injuries = create_injuries_dataframe(parsed_data)
+
+print(df_players.head())
+print(df_transfers.head())
+print(df_injuries.head())
+
+
+# get_weather_forecast()
--- data/players.py
+++ data/players.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from understatapi import UnderstatClient
+import ast
+
 understat = UnderstatClient()
 
 
@@ -12,3 +14,37 @@ def league_player_data(league, year):
     league_data = understat.league(league=str(
         league)).get_player_data(season=str(year))
     return pd.DataFrame(league_data)
+
+
+def parse_data(data_str):
+    return ast.literal_eval(data_str)
+
+
+def create_players_dataframe(data):
+    players_info = [
+        [player[0], player[1].strip(), player[2], player[3], player[4], player[5],
+         player[6].strip(), player[7], player[8], player[9]]
+        for player in data
+    ]
+    return pd.DataFrame(players_info, columns=['ID', 'Name', 'Team', 'Value', 'DOB', 'Rating', 'Height', 'Nationality', 'Position', 'Foot'])
+
+
+def create_transfers_dataframe(data):
+    transfers = []
+    for player in data:
+        player_id = player[0]
+        for transfer in player[10]:
+            from_club, to_club, season, date = transfer
+            transfers.append([player_id, from_club.strip(),
+                             to_club.strip(), season, date])
+    return pd.DataFrame(transfers, columns=['Player ID', 'From Club', 'To Club', 'Season', 'Date'])
+
+
+def create_injuries_dataframe(data):
+    injuries = []
+    for player in data:
+        player_id = player[0]
+        for injury in player[11]:
+            season, description, days = injury
+            injuries.append([player_id, season, description.strip(), days])
+    return pd.DataFrame(injuries, columns=['Player ID', 'Season', 'Injury', 'Days Missed'])
